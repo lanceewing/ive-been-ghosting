1ae51387dde64ec5cac4619e64aa39265ce8e22a
Lance Ewing <lance.ewing@gmail.com>
2022-09-02 09:54:47 +1200

    Added ect to pack-zip.sh as another zip tool for comparison.

--- pack-zip.sh
+++ pack-zip.sh
@@ -5,4 +5,5 @@ cp ../min.html index.html
 rm *.zip
 ../tools/kzip.exe //b512 min_kzip.zip index.html
 ../tools/advzip.exe -a -4 -i 10000 min_advzip.zip index.html
+../node_modules/ect-bin/vendor/win32/ect.exe -9 -strip -zip min_ect.zip index.html
 cd ..
